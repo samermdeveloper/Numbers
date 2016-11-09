cdb0809a6ca1fba8f4c246410e887a6da56f484b
Samer Mujibuddin <WorkStation@samers-mbp.attlocal.net>
2016-11-09 10:54:57 -0600

    Added files

---
+++ Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+//Integer
+var myBankAccount: Int = -500
+
+var myAge: UInt = 22
+//unsigned integers can only be used for values 0 and onward. (UInt)
+
+var bigNumber: Int64 = 21474836477555
+
+
+var aNum = 5
+
+var anotherBankAccount: Double = 55.5
+
+var someVal: Float = 5.5
+
+var sum = Float(anotherBankAccount) * someVal
+
+var bVal: Float = 5.6
+
+
+sum = bVal
+
+
